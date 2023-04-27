21dd193584953c5a08d041fa48806cbce7b1b716
Tomasz Janicki <tomasz.janicki@inetum.com>
2023-04-27 08:48:02 +0200

    one more example

---
+++ net7/Code/Presentation/E10ProgramFlow/BadExample.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Code.Presentation.E10ProgramFlow;
+
+public sealed class BadExample
+{
+    public sealed class Service
+    {
+        private readonly ILogger<Service> _logger;
+        private readonly IRepository _repository;
+
+        public Service(ILogger<Service> logger, IRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task ProcessCollection()
+        {
+            try
+            {
+                var items = await _repository.GetItems();
+                foreach (var item in items)
+                {
+                    try
+                    {
+                        var result = DoRequiredNotOptionalProcessing(item);
+
+                        await _repository.SaveItem(result);
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, "sth happened");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "sth happened");
+            }
+        }
+
+        private string DoRequiredNotOptionalProcessing(string input)
+        {
+            try
+            {
+                return input;
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "sth happened");
+
+                return string.Empty;
+            }
+        }
+
+        public async Task ProcessItem()
+        {
+            try
+            {
+                var item = await _repository.GetItem();
+                var result = DoRequiredNotOptionalProcessing(item);
+                await _repository.SaveItem(result);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "sth happened");
+            }
+        }
+
+    }
+
+    public interface IRepository
+    {
+        Task<IReadOnlyCollection<string>> GetItems();
+
+        Task<string> GetItem();
+
+        Task SaveItem(string data);
+    }
+}
---
+++ net7/Code/Presentation/E10ProgramFlow/GoodExample.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Code.Presentation.E10ProgramFlow;
+
+public sealed class GoodExample
+{
+    public sealed class Service
+    {
+        private readonly ILogger<Service> _logger;
+        private readonly IRepository _repository;
+
+        public Service(ILogger<Service> logger, IRepository repository)
+        {
+            _logger = logger;
+            _repository = repository;
+        }
+
+        public async Task ProcessCollection()
+        {
+            var items = await _repository.GetItems();
+            foreach (var item in items)
+            {
+                try
+                {
+                    var result = DoRequiredNotOptionalProcessing(item);
+
+                    await _repository.SaveItem(result);
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e, "sth happened");
+                }
+            }
+        }
+
+        private string DoRequiredNotOptionalProcessing(string input)
+        {
+            return input;
+        }
+
+        public async Task ProcessItem()
+        {
+            var item = await _repository.GetItem().ConfigureAwait(false);
+            var result = DoRequiredNotOptionalProcessing(item);
+            await _repository.SaveItem(result).ConfigureAwait(false);
+        }
+    }
+
+    public interface IRepository
+    {
+        Task<IReadOnlyCollection<string>> GetItems();
+
+        Task<string> GetItem();
+
+        Task SaveItem(string data);
+    }
+}
