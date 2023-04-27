44b8172bd91964fd42625f131df4d0db62aa35b7
Tomasz Janicki <tomasz.janicki@inetum.com>
2023-04-27 09:36:52 +0200

    fixes

--- net7/Code/Infrastructure/MessagePack/Resolvers/CustomFormatterResolverGetHelper.cs
+++ net7/Code/Infrastructure/MessagePack/Resolvers/CustomFormatterResolverGetHelper.cs
@@ -1,14 +1,39 @@
-﻿using Code.Infrastructure.MessagePack.Formatters;
+﻿using System.Collections.Concurrent;
+using System.Reflection;
+using Code.Infrastructure.MessagePack.Formatters;
 using MessagePack.Formatters;
 
 namespace Code.Infrastructure.MessagePack.Resolvers;
 
 public static class CustomFormatterResolverGetHelper
 {
-    private static readonly Dictionary<Type, object> FormatterMap = new()
-    {
-        {typeof(IReadOnlyCollection<int>), new ReadOnlyCollectionMessagePackFormatter<int>()}
-    };
+    private static readonly ConcurrentDictionary<Type, object> FormatterMap = new();
 
-    public static IMessagePackFormatter<T>? GetFormatter<T>() => FormatterMap.TryGetValue(typeof(T), out var formatter) ? (IMessagePackFormatter<T>)formatter : null;
+    public static IMessagePackFormatter<T>? GetFormatter<T>()
+    {
+        var type = typeof(T);
+        if (FormatterMap.TryGetValue(type, out var obj))
+        {
+            return (IMessagePackFormatter<T>)obj;
+        }
+        if (!type.IsGenericType)
+        {
+            return null;
+        }
+        if (type.GetGenericTypeDefinition() != typeof(IReadOnlyCollection<>))
+        {
+            return null;
+        }
+        var types = type.GetGenericArguments();
+        var formatterType = typeof(ReadOnlyCollectionMessagePackFormatter<>).MakeGenericType(types[0]);
+        var instance = Activator.CreateInstance(
+            formatterType,
+            BindingFlags.Instance | BindingFlags.Public,
+            binder: null,
+            args: Array.Empty<object>(),
+            culture: null)!;
+        FormatterMap.TryAdd(type, instance);
+
+        return (IMessagePackFormatter<T>)instance;
+    }
 }
